5ce300da6a3cfadfa87eba7a542ec915fefd2a83
Rohit <93656009+itisrohit@users.noreply.github.com>
2025-02-04 15:37:09 +0530

    Update restaurantSchema.js

--- models/restaurantSchema.js
+++ models/restaurantSchema.js
@@ -3,10 +3,10 @@ const mongooose = require('mongoose')
 const restaurentSchema = new mongoose.Schema({
     name: {type: String, required: true},
     city: {type:String, required: true},
-    itemArr: {
+    itemArr: [{
         type: mongooose.Schema.Types.ObjectId,
         ref: Item
-    }
+    }]
 })
 
 module.exports = mongooose.model('Restaurant', restaurentSchema)
